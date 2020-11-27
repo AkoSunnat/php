b55a84a88d0b833970cd9432b31c73ce953227b4
sunnat <savrulloevsunnatjon@gmail.com>
2020-11-27 05:21:37 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    $host = "remotemysql.com";
+    $db = "1z9Eejw0qq";
+    $user = "1z9Eejw0qq";
+    $pass = "HCtbQ6v2nB";
+    $charset = "utf8mb4";
+    $conn = new mysqli($host,$user,$pass);
+    $query = "SELECT id_customer,first_name,last_name FROM 1z9Eejw0qq.customer";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+          echo "id: " . $row["id_customer"]. " - Name: " . $row["first_name"]. " " . $row["last_name"]. "<br>";
+        }
+      } else {
+        echo "0 results\n";
+      }
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+?>
