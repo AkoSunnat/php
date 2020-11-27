73cb485dc21d9d06843e66a12ddfb81fffafb973
Sunnat <75102133+AkoSunnat@users.noreply.github.com>
2020-11-27 04:20:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,16 @@
     $pass = "HCtbQ6v2nB";
     $charset = "utf8mb4";
     $conn = new mysqli($host,$user,$pass);
+    $query = "SELECT id_customer,first_name,last_name FROM 1z9Eejw0qq.customer";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+          echo "id: " . $row["id_customer"]. " - Name: " . $row["first_name"]. " " . $row["last_name"]. "<br>";
+        }
+      } else {
+        echo "0 results";
+      }
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
