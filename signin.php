c6d04edca5ed459a6cc8c80446437e73109058e8
Sunnat <75102133+AkoSunnat@users.noreply.github.com>
2020-11-28 02:57:21 +0300

    Update signin.php

--- signin.php
+++ signin.php
@@ -17,7 +17,7 @@ if ($data[3] == $email){
     $query = "SELECT *FROM 1z9Eejw0qq.customer WHERE pass LIKE '$pass'";
     $res = mysqli_query($conn,$query);
     $data = mysqli_fetch_array($res);
-    //echo $data[5];
+    echo $data[5];
     if ($data[5] == $password){
         echo json_encode("true");
     }else{
