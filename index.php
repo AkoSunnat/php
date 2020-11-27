846dfd2f2745786f24987496ef153434b91a4621
Sunnat <75102133+AkoSunnat@users.noreply.github.com>
2020-11-27 23:01:01 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,6 @@
     $user = "1z9Eejw0qq";
     $pass = "HCtbQ6v2nB";
     $conn = new mysqli($host,$user,$pass);
-    $result = $conn->query($query);
-    if ($result->num_rows > 0) {
-        // output data of each row
-      } else {
-        echo "0 results\n";
-      }
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
