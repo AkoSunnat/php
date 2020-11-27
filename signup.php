fb20258036b292866372f429bbe254fb91567000
Sunnat <75102133+AkoSunnat@users.noreply.github.com>
2020-11-28 00:11:50 +0300

    Update signup.php

--- signup.php
+++ signup.php
@@ -5,19 +5,12 @@
     $first_name = $_POST["first_name"];
     $last_name = $_POST["last_name"];
     $phone_number = $_POST["phone_number"];
-    $query = "SELECT *FROM 1z9Eejw0qq.customer WHERE email LIKE savrulloevsunnatjon@gmail.com";
-    echo $email;
-    $res = $res->$conn->query($query);
-    $data = $res -> fetch_array(MYSQLI_NUM);
-    if ($data[3]>1){
-        echo json_encode("account already exists");
+    $query = "INSERT INTO 1z9Eejw0qq.customer(first_name,last_name,email,phone_number,password) VALUES ('$first_name','$last_name','$email','$phone_number','$password')";
+    $res = $conn->($query);
+    if ($res === TRUE){
+        echo json_encode("true");
     }else{
-        $query = "INSERT INTO 1z9Eejw0qq.customer(first_name,last_name,email,phone_number,password) VALUES ('$first_name','$last_name','$email','$phone_number','$password')";
-        $res = $conn->($query);
-        if ($res){
-            echo json_encode("true");
-        }else{
-            echo json_encode("false");
-        }
+        echo json_encode("false");
+      }
     }
 ?>
