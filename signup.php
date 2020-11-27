6e2b242da239f83b575a3d75767a1d6f95a6ce63
Sunnat <75102133+AkoSunnat@users.noreply.github.com>
2020-11-27 23:09:33 +0300

    Update signup.php

--- signup.php
+++ signup.php
@@ -8,6 +8,7 @@
     $query = "SELECT *FROM 1z9Eejw0qq.customer WHERE email LIKE '$email'";
     $res = mysqli_query($conn,$query);
     $data = mysqli_fetch_array($res);
+    echo $data[3];
     if ($data[3]>1){
         echo json_encode("account already exists");
     }else{
