dd70527bb821f2bc468a6bc2768b8e97c64144e7
Sunnat <75102133+AkoSunnat@users.noreply.github.com>
2020-11-28 02:36:51 +0300

    Add files via upload

---
+++ signin.php
@@ -0,0 +1,28 @@
+<?php
+$host = "remotemysql.com";
+$db = "1z9Eejw0qq";
+$user = "1z9Eejw0qq";
+$pass = "HCtbQ6v2nB";
+$conn = mysqli_connect($host, $user, $pass);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$email = $_POST["email"];
+$password = $_POST["password"];
+$query = "SELECT *FROM 1z9Eejw0qq.customer WHERE email LIKE '$email'";
+$res = mysqli_query($conn,$query);
+$data = mysqli_fetch_array($res);
+if ($data[3] == $email){
+    $query = "SELECT *FROM 1z9Eejw0qq.customer WHERE pass LIKE '$pass'";
+    $res = mysqli_query($conn,$query);
+    $data = mysqli_fetch_array($res);
+    if ($data[5] == $pass){
+        echo json_encode("true");
+    }else{
+        echo json_encode("false");
+    }
+}else{
+    echo json_encode("account doesnt exist");
+}
+?>
