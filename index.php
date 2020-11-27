38c6cf7df15b794da259caad96439d152ad6c640
Sunnat <75102133+AkoSunnat@users.noreply.github.com>
2020-11-28 00:28:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
     $db = "1z9Eejw0qq";
     $user = "1z9Eejw0qq";
     $pass = "HCtbQ6v2nB";
-    $conn = new mysqli($host,$user,$pass);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+    $conn = mysqli_connect($host, $user, $pass);
+// Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
     }
+    echo "Connected successfully";
 ?>
