6e68760537f4ea19ce33e10b99dbb31bf7269602
Sunnat <75102133+AkoSunnat@users.noreply.github.com>
2020-11-28 00:23:48 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     $user = "1z9Eejw0qq";
     $pass = "HCtbQ6v2nB";
     $conn = new mysqli($host,$user,$pass);
-    echo "bitch";
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
