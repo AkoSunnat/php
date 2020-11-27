f58afa1bf0c31eb01fe9d499560b56e32a573702
Sunnat <75102133+AkoSunnat@users.noreply.github.com>
2020-11-27 19:47:13 +0300

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,22 @@
+<?php
+    require_once("index.php");
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $first_name = $_POST["first_name"];
+    $last_name = $_POST["last_name"];
+    $phone_number = $_POST["phone_number"];
+    $query = "SELECT *FROM 1z9Eejw0qq.customer WHERE email LIKE '$email'";
+    $res = mysqli_query($conn,$query);
+    $data = mysqli_fetch_array($res);
+    if ($data[3]>1){
+        echo json_encode("account already exists");
+    }else{
+        $query = "INSERT INTO 1z9Eejw0qq.customer(first_name,last_name,email,phone_number) VALUES ('$first_name','$last_name','$email','$phone_number')";
+        $res = mysqli_query($query);
+        if ($res){
+            echo json_encode("true");
+        }else{
+            echo json_encode("false");
+        }
+    }
+?>
