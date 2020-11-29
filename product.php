793324856ebb8dd79aa81289baed58956bbdc007
Sunnat <75102133+AkoSunnat@users.noreply.github.com>
2020-11-29 03:47:49 +0300

    Add files via upload

---
+++ product.php
@@ -0,0 +1,15 @@
+<?php
+$host = "remotemysql.com";
+$db = "1z9Eejw0qq";
+$user = "1z9Eejw0qq";
+$pass = "HCtbQ6v2nB";
+$conn = new mysqli($host, $user, $pass);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$query = "SELECT *FROM 1z9Eejw0qq.product";
+$res = $conn->query($query);
+$result = json_encode($res->fetch_assoc());
+echo $result;
+?>
