13265caf0ee06f04a8558622bdf22c730021b1cc
Sunnat <75102133+AkoSunnat@users.noreply.github.com>
2020-12-02 11:02:10 +0300

    Add files via upload

---
+++ issuing_pointadd.php
@@ -0,0 +1,30 @@
+
+<?php
+$host = "remotemysql.com";
+$db = "1z9Eejw0qq";
+$user = "1z9Eejw0qq";
+$pass = "HCtbQ6v2nB";
+$conn = mysqli_connect($host, $user, $pass);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$city = $_POST["city"];
+$street = $_POST["street"];
+$house = $_POST["house"];
+$query = "SELECT *FROM 1z9Eejw0qq.issueing_point WHERE city LIKE '$city' AND street LIKE '$street' AND house LIKE '$house'";
+$res = mysqli_query($conn,$query);
+$data = mysqli_fetch_array($res);
+
+if ($data[3]==$house && $data[2]==$street && $data[1]==$city){
+    echo json_encode("place already exists");
+}else{
+    $query = "INSERT INTO 1z9Eejw0qq.issueing_point(city,street,house) VALUES ('$city','$street','$house')";
+    $res = mysqli_query($conn,$query);
+    if ($res){
+        echo json_encode("true");
+    }else{
+        echo json_encode("false");
+    }
+}
+?>
