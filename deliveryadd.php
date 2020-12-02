af1aa45e5e21b3aa4a70faa88345c3cb503d5f96
Sunnat <75102133+AkoSunnat@users.noreply.github.com>
2020-12-02 08:59:37 +0300

    Update deliveryadd.php

--- deliveryadd.php
+++ deliveryadd.php
@@ -4,13 +4,14 @@ $db = "1z9Eejw0qq";
 $user = "1z9Eejw0qq";
 $pass = "HCtbQ6v2nB";
 $conn = new mysqli($host, $user, $pass);
-$first_name = $_POST["name"];
-$last_name = $_POST["surname"];
+$name = $_POST["name"];
+$surname = $_POST["surname"];
 $phone_number = $_POST["phone_number"];
 $query = "SELECT *FROM 1z9Eejw0qq.delievery_man WHERE phone_number LIKE '$phone_number'";
 $res = mysqli_query($conn,$query);
 $data = mysqli_fetch_array($res);
-if ($data[3]>1){
+echo $surname;
+if ($data[3]==$phone_number){
     echo json_encode("account already exists");
 }else{
     $query = "INSERT INTO 1z9Eejw0qq.delievery_man(name,surname,phone_number) VALUES ('$first_name','$last_name','$phone_number')";
