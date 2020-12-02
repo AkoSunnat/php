3f33ce9d42cb61dd893674fca46dae5bd0e02ab6
Sunnat <75102133+AkoSunnat@users.noreply.github.com>
2020-12-02 09:11:42 +0300

    Update deliveryadd.php

--- deliveryadd.php
+++ deliveryadd.php
@@ -1,5 +1,13 @@
 <?php
-require_once("index.php");
+$host = "remotemysql.com";
+$db = "1z9Eejw0qq";
+$user = "1z9Eejw0qq";
+$pass = "HCtbQ6v2nB";
+$conn = mysqli_connect($host, $user, $pass);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 $name = $_POST["name"];
 $surname = $_POST["surname"];
 $phone_number = $_POST["phone_number"];
