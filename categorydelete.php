7838e139a2059647abbfa732c940f6041d77117b
Sunnat <75102133+AkoSunnat@users.noreply.github.com>
2020-12-03 04:41:20 +0300

    Update categorydelete.php

--- categorydelete.php
+++ categorydelete.php
@@ -15,8 +15,9 @@ $query = "SELECT *FROM 1z9Eejw0qq.category WHERE cat_name LIKE '$name'";
 $res = mysqli_query($conn,$query);
 $data = mysqli_fetch_array($res);
 if ($data[1] == $name){
-    $sql = "DELETE FROM 1z9Eejw0qq.category WHERE cat_name = '$name';ALTER TABLE 1z9Eejw0qq.category AUTO_INCREMENT = 1";
+    $sql = "DELETE FROM 1z9Eejw0qq.category WHERE cat_name = '$name'";
     $result = mysqli_query($conn,$sql);
+    mysqli_query($conn,"ALTER TABLE 1z9Eejw0qq.category AUTO_INCREMENT = 1");
     if ($result){
         echo json_encode("true");
     }else{
