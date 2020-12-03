7de618450c5da658ad35cfec784072030267f530
Sunnat <75102133+AkoSunnat@users.noreply.github.com>
2020-12-03 06:33:52 +0300

    Add files via upload

---
+++ productadd.php
@@ -0,0 +1,30 @@
+<?php
+$host = "remotemysql.com";
+$db = "1z9Eejw0qq";
+$user = "1z9Eejw0qq";
+$pass = "HCtbQ6v2nB";
+$conn = new mysqli($host, $user, $pass);
+$prod_name = $_POST["prod_name"];
+$brand = $_POST["brand"];
+$category = $_POST["category_id"];
+$size = $_POST["size"];
+$color = $_POST["color"];
+$price = $_POST["price"];
+$quantity = $_POST["quantity"];
+$img_url = $_POST["img_url"];
+$query = "SELECT *FROM 1z9Eejw0qq.product WHERE prod_name LIKE '$prod_name' AND brand LIKE '$brand' AND category_id LIKE '$category' AND size LIKE '$size' AND color LIKE '$color' AND price LIKE '$price'";
+$res = mysqli_query($conn,$query);
+$data = mysqli_fetch_array($res);
+if ($data[1]==$prod_name && $data[2]==$brand && $data[3]==$category && $data[4]==$size && $data[5]==$color && $data[6]==$price){
+    echo json_encode("product already exists");
+}else{
+    $query = "INSERT INTO 1z9Eejw0qq.product(prod_name,brand,category_id,size,color,price,quantity,img_url) VALUES ('$prod_name','$brand','$category','$size','$color','$price','$quantity','$img_url')";
+    $res = mysqli_query($conn,$query);
+    mysqli_query($conn,"ALTER TABLE 1z9Eejw0qq.product AUTO_INCREMENT = 1");
+    if ($res){
+        echo json_encode("true");
+    }else{
+        echo json_encode("false");
+    }
+}
+?>
