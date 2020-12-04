94609505c54173f2fc936114fc4d7ac8fc625b84
Sunnat <75102133+AkoSunnat@users.noreply.github.com>
2020-12-04 05:11:27 +0300

    Update customergetid.php

--- customergetid.php
+++ customergetid.php
@@ -12,9 +12,9 @@ $email = $_POST["email"];
 $password = $_POST["password"];
 
 $query = "SELECT 1z9Eejw0qq.customer.id_customer FROM 1z9Eejw0qq.customer WHERE 1z9Eejw0qq.customer.email LIKE '$email' AND 1z9Eejw0qq.customer.password LIKE '$password'";
-echo $email;
 $res = mysqli_query($conn,$query);
 $data = mysqli_fetch_array($res);
+echo $data[0];
 if($data[0]>1){
     echo json_encode($data[0]);
 }else{
